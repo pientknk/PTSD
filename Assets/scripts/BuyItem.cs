1e11d57ce24101c8e75e08b6be1a5f67a0ad1fed
pientknk <pientknk@uwec.edu>
2017-03-07 11:02:17 -0600

    did everything

---
+++ Assets/scripts/BuyItem.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(TheBank))]
+public class BuyItem : MonoBehaviour {
+
+	private TheBank bank;
+	private bool canBuy;
+	private string thisTag;
+	private Button button;
+	// Use this for initialization
+	void Start () {
+		bank = GetComponent<TheBank> ();
+		thisTag = this.tag;
+		button = GetComponent<Button>();
+	}
+
+	public void buyItem(){
+		switch (thisTag){
+		case "Conveyor":
+			bank.subtractMoney (bank.ConveyorCost);
+			break;
+		case "Trampoline":
+			bank.subtractMoney (bank.TrampolineCost);
+			break;
+		case "Slide":
+			bank.subtractMoney (bank.SlideCost);
+			break;
+		case "Fan":
+			bank.subtractMoney (bank.FanCost);
+			break;
+		case "Filter":
+			bank.subtractMoney (bank.FilterCost);
+			break;
+		default:
+			print ("Error: " + thisTag + " - must have an appropriate tag. e.g. Conveyor");
+			break;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch (thisTag){
+		case "Conveyor":
+			if (!bank.canBuyConveyor ()) {
+				button.interactable = false;
+			}
+			break;
+		case "Trampoline":
+			if (!bank.canBuyTrampoline ()) {
+				button.interactable = false;
+			}
+			break;
+		case "Slide":
+			if (!bank.canBuySlide ()) {
+				button.interactable = false;
+			}
+			break;
+		case "Fan":
+			if (!bank.canBuyFan ()) {
+				button.interactable = false;
+			}
+			break;
+		case "Filter":
+			if (!bank.canbBuyFilter ()) {
+				button.interactable = false;
+			}
+			break;
+		default:
+			print ("Error: " + thisTag + " - must have an appropriate tag. e.g. Conveyor");
+			break;
+		}
+	}
+}
---
+++ Assets/scripts/updateItemCost.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(TheBank))]
+public class updateItemCost : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		TheBank bank = GetComponent<TheBank> ();
+		Text label = GetComponent<Text> ();
+		switch (this.tag){
+		case "Conveyor":
+			label.text = "Conveyor - $" + bank.ConveyorCost;
+			break;
+		case "Trampoline":
+			label.text = "Trampoline - $" + bank.TrampolineCost;
+			break;
+		case "Slide":
+			label.text = "Slide - $" + bank.SlideCost;
+			break;
+		case "Fan":
+			label.text = "Fan - $" + bank.FanCost;
+			break;
+		case "Filter":
+			label.text = "Filter - $" + bank.FilterCost;
+			break;
+		default:
+			label.text = "Unknown";
+			print ("Error: " + this.tag + " - must have an appropriate tag. e.g. Conveyor");
+			break;
+		}
+	}
+	
+}
