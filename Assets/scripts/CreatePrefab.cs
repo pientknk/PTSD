1f2f9e3a2183096a1199427bbf9ef6892d53e377
pientknk <pientknk@uwec.edu>
2017-03-07 11:22:43 -0600

    Merge branch 'nickpientka'
    # Conflicts:
    #	Assets/CreateSnapPoints.cs.meta
    #	Assets/Prefabs/Cell.prefab.meta
    #	Assets/Prefabs/InventoryPanel 1.prefab.meta
    #	Assets/Prefabs/Item.prefab.meta
    #	Assets/Prefabs/Usable Conveyor.prefab.meta
    #	Assets/StarlinkUI.meta
    #	Assets/materials.meta
    #	Assets/materials/blue.mat
    #	Assets/materials/blue.mat.meta
    #	Assets/scene1.unity
    #	Assets/scripts/CalcForce.cs.meta
    #	Assets/scripts/ConveyorSpeed.cs
    #	Assets/scripts/ConveyorSpeed.cs.meta
    #	Assets/scripts/CreatePrefab.cs.meta
    #	Assets/scripts/DummyControlUnit.cs.meta
    #	Assets/scripts/TheBank.cs.meta
    #	Assets/scripts/TogglePanelButton.cs.meta
    #	Assets/scripts/updateBank.cs.meta
    #	Assets/sprites/Conveyor.anim.meta
    #	Assets/sprites/UIImages.meta
    #	Assets/sprites/UIPanel.png
    #	Assets/sprites/UIToggleButton.png
    #	Assets/sprites/conveyorBelt1.png
    #	Assets/sprites/conveyorBelt1.png.meta
    #	ProjectSettings/TagManager.asset

---
+++ Assets/scripts/CreatePrefab.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatePrefab : MonoBehaviour {
+
+	// Use this for initialization
+	public void createPrefab(GameObject prefab){
+		GameObject clone = Instantiate(prefab) as GameObject;
+		Canvas canvas = GameObject.FindGameObjectWithTag("Canvas").GetComponent<Canvas>();
+		if (canvas != null) {
+			clone.transform.SetParent (canvas.transform);
+			clone.transform.localPosition = new Vector2(0.0f, 0.0f);
+			RectTransform cloneRect = GetComponent<RectTransform> ();
+			//check tag of object and make it the appropriate size
+			switch (this.tag){
+			case "Conveyor":
+				//clone.transform.localScale = new Vector3 (2.0f, 2.0f);
+				cloneRect.sizeDelta = new Vector2 (100f, 40f);
+				break;
+			case "Trampoline":
+				break;
+			case "Slide":
+				break;
+			case "Fan":
+				break;
+			case "Filter":
+				break;
+			default:
+				print ("Error: unable to create an object in CreatePrefab.cs");
+				break;
+			}
+
+		}
+
+	}
+}
---
+++ Assets/scripts/updateBank.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(TheBank))]
+public class updateBank : MonoBehaviour {
+
+	private TheBank bank;
+	private Text label;
+	// Use this for initialization
+	void Start () {
+		bank = GetComponent<TheBank> () as TheBank;
+		label = GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		label.text = "FUNDS: $" + bank.CurrentMoney;
+	}
+}
