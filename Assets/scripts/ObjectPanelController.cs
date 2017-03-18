79403d72a5e48b12f19fc0470919b654d515cb77
pientknk <pientknk@uwec.edu>
2017-03-18 01:45:47 -0500

    Changed file structure and added new assets including a modify button and object panel v2 along with some new scripts to go with them

--- Assets/scripts/ObjectPanelController.cs
+++ Assets/scripts/ObjectPanelController.cs
@@ -1,18 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-[RequireComponent(typeof(ShowOrHideSubMenu))]
-[RequireComponent(typeof(rotateObject))]
+public class ObjectPanelController : MonoBehaviour{
 
-public class ObjectPanelController : MonoBehaviour {
-
-	private ShowOrHideSubMenu subMenu;
+	private GameObject objectPanel;
 	// Use this for initialization
 	void Start () {
-		
+		RectTransform usableTransform = gameObject.GetComponent<RectTransform> ();
+		float height = usableTransform.rect.height / 2;
+		float width = usableTransform.rect.width / 2;
+		GameObject panel = GameObject.FindGameObjectWithTag ("Canvas");
+		objectPanel = (GameObject)Instantiate(Resources.Load("Prefabs/UI/Object Panel v2"), 
+								new Vector3(transform.position.x + width + 4, transform.position.y - height,
+								transform.position.z), Quaternion.identity, panel.transform);
+		if (objectPanel != null) {
+			// set panels transform to right position and set inactive initially
+			RectTransform objectRect = objectPanel.GetComponent<RectTransform> ();
+			float objHeight = objectRect.rect.height / 2;
+			Vector3 objVector = new Vector3 (objectPanel.transform.position.x, 
+				objectPanel.transform.position.y - objHeight, 
+				objectPanel.transform.position.z);
+			objectPanel.transform.position = objVector;
+			objectPanel.SetActive (false);
+
+		} else {
+			print ("Couldn't load object in ObjectPanelController.cs from specified directory");
+		}
+
 	}
-	
+
+	public void togglePanel(){
+		if (objectPanel != null) {
+			objectPanel.SetActive (!objectPanel.activeSelf);
+		} else {
+			print ("Can't change active state of a null object in ObjectPanelController.cs");
+		}
+
+	}
+
 	// Update is called once per frame
 	void Update () {
 		
---
+++ Assets/scripts/UpdateObjectPanelLabel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateObjectPanelLabel : MonoBehaviour {
+
+	private Text label;
+	private string objectSelected;
+	public string ObjectSelected{
+		get{ return objectSelected; }
+		set{ objectSelected = value; }
+	}
+
+	// Use this for initialization
+	void Start () {
+		objectSelected = "None";
+		label = GetComponent<Text> ();
+		if (label == null) {
+			print ("No text component found on object with UpdateObjectPanelLabel.cs attached");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (label != null) {
+			label.text = "Selected: " + objectSelected;
+		}
+	}
+}
