5deb7b4ddf362e58b3b2ab3e1fcb9240c6314bb9
Nick Petersson <petersso@uwec.edu>
2017-03-19 19:51:13 -0500

    Camera Zoom and Move
    Good progress on movement. Still need more work on setting movement
    boundary (which gets difficult with zoom) and not moving when an item is
    selected

---
+++ Assets/scripts/CameraMove.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour {
+
+	private float camX;
+	private float camY;
+
+	private float xMax;
+	private float yMax;
+	private float xMin = 200.0f;
+	private float yMin = 130.0f;
+
+	private float xDiff;
+	private float yDiff;
+
+	private Vector3 anchor;
+	private Vector3 fromAnchor;
+	private Vector3 newPos;
+
+	private float baseOrth;
+	private float newOrth;
+
+	void Start() {
+		camX = transform.position.x;
+		camY = transform.position.y;
+		newPos = transform.position;
+		baseOrth = gameObject.GetComponent<Camera> ().orthographicSize;
+
+		xMax = (transform.position.x * baseOrth);
+		yMax = (transform.position.y * baseOrth);
+	}
+
+	void Update() {
+
+
+		if(Input.GetMouseButtonDown(0)) {
+			anchor = Input.mousePosition;
+		}
+
+		if(Input.GetMouseButton(0)) {
+			fromAnchor = Input.mousePosition;
+			newOrth = gameObject.GetComponent<Camera> ().orthographicSize;
+
+			//without shrinking the difference the movement is way too fast
+			xDiff = (anchor.x - fromAnchor.x)/40.0f;
+			yDiff = (anchor.y - fromAnchor.y)/40.0f;
+
+			if(((camX + xDiff) * newOrth < xMax) && (camX + xDiff > xMin)) {
+				camX += xDiff;
+			}
+
+			if ((((camY + yDiff) * newOrth) < yMax) && (camY + yDiff > yMin)) {
+				camY += yDiff;
+			}
+				
+			newPos.x = camX;
+			newPos.y = camY;
+		}
+			
+		transform.position = newPos;
+		//print ("New Pos: " + newPos + ", yMax: " + yMax + ", curr Y: " + camY*newOrth + ", Base Orth: " + baseOrth + ", New Orth: " + gameObject.GetComponent<Camera>().orthographicSize);
+
+	}
+
+}
---
+++ Assets/scripts/Zoom.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zoom : MonoBehaviour {
+
+	private float yMax;
+	Vector3 camPos;
+
+	private float maxOrth;
+	private float minOrth = 30.0f;
+
+	private float newOrth;
+
+	// Use this for initialization
+	void Start () {
+		newOrth = gameObject.GetComponent<Camera> ().orthographicSize;
+		maxOrth = gameObject.GetComponent<Camera> ().orthographicSize;
+
+		yMax = maxOrth * transform.position.y;
+
+		camPos.x = transform.position.x;
+		camPos.z = transform.position.z;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		camPos = transform.position;
+
+		if (Input.GetAxis ("Mouse ScrollWheel") < 0) {
+			if (newOrth < maxOrth) {
+				newOrth += 7.0f;
+			}
+		} else if (Input.GetAxis ("Mouse ScrollWheel") > 0) {
+			if (newOrth > minOrth) {
+				newOrth -= 7.0f;
+			}
+		}
+
+		if ((newOrth * camPos.y) > yMax) {
+			camPos.y -= 70.0f;
+			gameObject.GetComponent<Camera> ().transform.position = camPos;
+		}
+
+		gameObject.GetComponent<Camera> ().transform.position = camPos;
+		gameObject.GetComponent<Camera> ().orthographicSize = newOrth;
+		print ("yMax: " + yMax + ", curr Y: " + camPos.y*newOrth + ", New Orth: " + newOrth + " yCam: " + gameObject.GetComponent<Camera>().transform.position.y);
+	}
+}
