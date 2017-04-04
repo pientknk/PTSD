dc7fc9a00e8604e140b816a25548f5fe8b01d086
pientknk <pientknk@uwec.edu>
2017-04-04 00:52:00 -0500

    Fixed destroy package script to update the level manager

--- Assets/scripts/LevelControllerScripts/DestroyPackage.cs
+++ Assets/scripts/LevelControllerScripts/DestroyPackage.cs
@@ -6,5 +6,16 @@ public class DestroyPackage : MonoBehaviour {
 		LevelController.instance.FailurePackages++;
 		LevelController.instance.currentMoney -= (int)LevelController.instance.packageWorth / 2;
 		Destroy (col.gameObject);
+		LevelController.instance.PackagesDestroyed++;
+		checkPackageDestructionCount ();
+
+	}
+
+	private void checkPackageDestructionCount(){
+		if (LevelController.instance.PackagesDestroyed == LevelController.instance.allPackages.Count) {
+			print ("Done with level");
+			LevelController.instance.summaryCanvas.SetActive (true);
+			LevelController.instance.canvas.GetComponent<CanvasGroup> ().interactable = false;
+		}
 	}
 }
