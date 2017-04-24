6f9de5501c1a97d8a1a110dce3cc84219f3fa0e0
Nick Petersson <petersso@uwec.edu>
2017-04-24 12:04:57 -0500

    Drag Panel work
    Will work on standard sized levels. Still working on figuring out how it
    will work on all levels, regardless of size.

--- Assets/scripts/DragPanel.cs
+++ Assets/scripts/DragPanel.cs
@@ -9,9 +9,33 @@ public class DragPanel : MonoBehaviour, IPointerDownHandler, IDragHandler {
 	private RectTransform canvasRectTransform;
 	private RectTransform panelRectTransform;
 
+	private int theScreenWidth;
+	private int theScreenHeight;
+
+	private float xMin;
+	private float xMax;
+	private float yMin;
+	private float yMax;
+
+	private GameObject theLevelObject;
+	private Vector3 levelObjectsRect;
+
 
 	void Start () {
 		GameObject canvas = LevelController.instance.canvas;
+
+		//theScreenWidth = Screen.width;
+		//theScreenHeight = Screen.height;
+		//theLevelObject = LevelController.instance.theLevelObjects;
+		//levelObjectsRect = theLevelObject.GetComponent<RectTransform> ().position;
+
+		//Hard coding in these values will not work on different sized levels.
+		//Still working on figuring out how to dynamically find boundaries
+		xMin = -710;
+		xMax = 485;
+		yMin = 235;
+		yMax = 438;
+
 		if (canvas != null) {
 			canvasRectTransform = canvas.transform as RectTransform;
 			panelRectTransform = transform.parent as RectTransform;
@@ -36,6 +60,12 @@ public class DragPanel : MonoBehaviour, IPointerDownHandler, IDragHandler {
 				panelRectTransform.localPosition = localPointerPosition - pointerOffset;
 			}
 		} else {
+
+			//xMin = levelObjectsRect.x - (theScreenWidth/2) + 600;
+			//xMax = levelObjectsRect.x + (theScreenWidth/2) + 600;
+			//yMin = levelObjectsRect.y - (theScreenHeight/2) + 70;
+			//yMax = levelObjectsRect.y + (theScreenHeight/2) + 70;
+
 				if (panelRectTransform == null)
 					return;
 
@@ -44,10 +74,29 @@ public class DragPanel : MonoBehaviour, IPointerDownHandler, IDragHandler {
 
 				Vector2 localPointerPosition;
 				if (RectTransformUtility.ScreenPointToLocalPointInRectangle (
-					canvasRectTransform, pointerPostion, data.pressEventCamera, out localPointerPosition
-				)) {
+				     canvasRectTransform, pointerPostion, data.pressEventCamera, out localPointerPosition
+			     )) {
+				
+				if ((panelRectTransform.localPosition.x > xMin) && (panelRectTransform.localPosition.x < xMax) && (panelRectTransform.localPosition.y < yMax) && (panelRectTransform.localPosition.y > yMin)) {
 					panelRectTransform.localPosition = localPointerPosition - pointerOffset;
 				}
+
+				if (panelRectTransform.localPosition.x < xMin) {
+					panelRectTransform.localPosition = new Vector3 ((xMin + 1), panelRectTransform.localPosition.y);
+				}
+
+				if (panelRectTransform.localPosition.x > xMax) {
+					panelRectTransform.localPosition = new Vector3 ((xMax - 1), panelRectTransform.localPosition.y);
+				}
+
+				if (panelRectTransform.localPosition.y > yMax) {
+					panelRectTransform.localPosition = new Vector3 (panelRectTransform.localPosition.x, (yMax - 1));
+				}
+
+				if (panelRectTransform.localPosition.y < yMin) {
+					panelRectTransform.localPosition = new Vector3 (panelRectTransform.localPosition.x, (yMin + 1));
+				}
+			}
 		}
 
 	}
