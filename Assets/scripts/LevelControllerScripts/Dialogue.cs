c7fd322aaf2aa6eb5754cc90c68145037f072fa7
pientknk <pientknk@uwec.edu>
2017-04-26 23:13:49 -0500

    added dialogue box, and started working on a tooltip

---
+++ Assets/scripts/LevelControllerScripts/Dialogue.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dialogue : MonoBehaviour {
+
+
+	public Text dialogue;
+	public Image moreTextIndicator;
+	public List<string> dialogueParts;
+	private int index = 0;
+	private float timer = 0.0f;
+	private float timeBetweenSwitch = 1.0f;
+	// Use this for initialization
+	void Start () {
+		dialogue.text = dialogueParts [index];
+		CheckForMoreText ();
+		LevelController.instance.canvas.GetComponent<CanvasGroup> ().interactable = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.anyKeyDown && index < dialogueParts.Count) {
+			index++;
+			if (index == dialogueParts.Count) {
+				LevelController.instance.canvas.GetComponent<CanvasGroup> ().interactable = true;
+				Destroy (gameObject);
+			} else {
+				ShowDialogue (index);
+				CheckForMoreText ();
+			}
+		}
+		timer += Time.unscaledDeltaTime;
+		if (timer >= timeBetweenSwitch) {
+			timer = 0.0f;
+			ToggleMoreTextIndicator ();
+		}
+	}
+
+	private void ShowDialogue(int index){
+		dialogue.text = dialogueParts [index];
+	}
+
+	private void CheckForMoreText(){
+		if (dialogueParts.Count > index + 1) {
+			moreTextIndicator.gameObject.SetActive (true);
+		} else {
+			moreTextIndicator.gameObject.SetActive (false);
+		}
+	}
+
+	private void ToggleMoreTextIndicator(){
+		if (dialogueParts.Count > index + 1) {
+			moreTextIndicator.gameObject.SetActive (!moreTextIndicator.gameObject.activeSelf);
+		}
+	}
+}
---
+++ Assets/scripts/LevelControllerScripts/ToolTip.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ToolTip : MonoBehaviour {
+
+	private static ToolTip instance;
+	public static ToolTip Instance {
+		get { 
+			if (instance == null) {
+				instance = GameObject.FindObjectOfType<ToolTip>();
+			}
+			return instance;
+		}
+	}
+
+	public bool IsActive{
+		get{ return gameObject.activeSelf; }
+	}
+
+	public Text tooltipText;
+
+	void Awake(){
+		instance = this;
+		HideTooltip ();
+	}
+
+	public void ShowTooltip(string text, Vector3 pos){
+		if (tooltipText.text != text) {
+			tooltipText.text = text;
+		}
+		transform.localPosition = pos;
+		gameObject.SetActive (true);
+	}
+
+	public void HideTooltip(){
+		gameObject.SetActive (false);
+	}
+
+}
