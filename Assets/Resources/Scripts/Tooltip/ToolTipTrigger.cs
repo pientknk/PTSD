8eab51ce7c9743f330743036a8771bd3c3ececff
pientknk <pientknk@uwec.edu>
2017-05-18 16:14:13 -0500

    fixed some tool tip issues on every level

--- Assets/Resources/Scripts/Tooltip/ToolTipTrigger.cs
+++ Assets/Resources/Scripts/Tooltip/ToolTipTrigger.cs
@@ -14,54 +14,18 @@ public class ToolTipTrigger : MonoBehaviour, IPointerEnterHandler, IPointerExitH
 
 	private bool shouldShow;
 	private Vector2 cameraPos;
-	private float leftEdge;
-	private float rightEdge;
-	private float topEdge;
-	private float bottomEdge;
-	private float leftEdgeBox;
-	private float rightEdgeBox;
-	private float bottomEdgeBox;
-	private float topEdgeBox;
-	private Rect tooltipRect;
-	private Vector2 tooltipCenter;
 
 	void Start(){
 		shouldShow = GameController.tooltip;
-		cameraPos = Camera.main.gameObject.transform.position;
-		leftEdge = cameraPos.x - (float)Screen.width / 2.0f;
-		rightEdge = cameraPos.x + (float)Screen.width / 2.0f;
-		topEdge = cameraPos.y + (float)Screen.height / 2.0f;
-		bottomEdge = cameraPos.y - (float)Screen.height / 2.0f;
-		if (toolTip != null) {
-			tooltipRect = toolTip.GetComponent<RectTransform> ().rect;
-		} else {
+		if (toolTip == null) {
 			print ("Tooltip not set for tool tip trigger on: " + gameObject.name);
-		}
+		} 
 	}
 
 	public void OnPointerEnter(PointerEventData eventData){
 		if (shouldShow) {
-			Vector3 pos = eventData.position; // x offset - 267, y offset - 251.8
-			Vector3 modPos = new Vector3 (pos.x - 267f, pos.y - 251.8f);
-			if (toolTip != null) {
-				// if left edge of tooltip is off screen, move right
-				if (modPos.x - tooltipRect.xMax < leftEdge) {
-					modPos.x = leftEdge + tooltipRect.xMax;
-				}
-				// if right edge of tooltip is off screen, move left
-				if (modPos.x + tooltipRect.xMax > rightEdge) {
-					modPos.x = rightEdge - tooltipRect.xMax;
-				}
-				// if top edge of tooltip is off screen, move down
-				if (modPos.y + tooltipRect.yMax > topEdge) {
-					modPos.y = topEdge - tooltipRect.yMax;
-				}
-				// if bottom edge of tooltip is off screen, move up
-				if (modPos.y - tooltipRect.yMax < bottomEdge) {
-					modPos.y = bottomEdge + tooltipRect.yMax;
-				}
-			}
-			StartHover (modPos);
+			Vector3 pos = eventData.position;
+			StartHover (pos);
 		}
 	}
 
